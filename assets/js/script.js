0fb745c9d490b383521f528e33b4e1530a51c966
jbradley84 <utbradley84@yahoo.com>
2022-05-18 15:16:12 -0500

    pseudocoded password generator process flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Establish password length - prompt user, 8 - 128 characters long
+
+// Establish password makeup
+/* Confirm user, lowercase: Y/N
+   Confirm user, uppercase: Y/N
+   Confirm user, numbers: Y/N
+   Confirm user, special characters: Y/N
+*/
+
+// Establish password character arrays
+
+// Concatenate TRUE statement character arrays
+
+// Use math.random to generate password - must pull characters from each TRUE array
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
