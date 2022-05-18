671698760b6457935115aa7d6b66195ce0207823
jbradley84 <utbradley84@yahoo.com>
2022-05-18 17:55:49 -0500

    added lowercase, uppercase, numeric, and special character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,50 @@
 // Assignment Code
 //var generateBtn = document.querySelector("#generate");
 
-// Establish password makeup
-var passwordLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8 and 128.")); {
+
+// Establish possible character arrays
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericValueArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharactersArray = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var emptyArray = [];
+
+
+// Establish password length between 8 and 128 characters
+var passwordLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8 and 128."))/*; {
     if (passwordLength < 8 || passwordLength > 128) {
       window.alert("You must choose a password length between 8 and 128 characters long!");
     }
-};
-var lowerCase = window.confirm("Do you want your password to contain lowercase letters?")
-var upperCase = window.confirm("Do you want your password to include uppercase letters?")
-var numericalValues = window.confirm("Do you want your password to contain numbers?")
+};*/
+
+
+// Establish password makeup
+var lowerCase = window.confirm("Do you want your password to contain lowercase letters?"); {
+  if (console.log(lowerCase) === true) {
+    passwordCharacterArray = emptyArray.concat(lowerCaseArray);
+  }
+}
+// If TRUE, concatenate lowerCaseArray with passwordCharacterArray
+
+
+// Establish if uppercase letters are wanted
+var upperCase = window.confirm("Do you want your password to include uppercase letters?"); {
+  if (console.log(upperCase) === true) {
+    passwordCharacterArray = passwordCharacterArray.concat(upperCaseArray);
+  }
+}
+// If TRUE, concatenate upperCaseArray with passwordCharacterArray
+
+
+// Establish if numbers are wanted
+var numericValue = window.confirm("Do you want your password to contain numbers?")
+// If TRUE, concatenate numericValueArray with passwordCharacterArray
+
+
+// Establish if special characters are wanted
 var specialCharacters = window.confirm("Do you want your password to contain special characters?")
+// If true, concatenate specialCharactersArray with passwordCharacterArray
+
 
 // Establish password character arrays
 
@@ -32,5 +66,6 @@ generateBtn.addEventListener("click", writePassword);*/
 console.log(passwordLength);
 console.log(lowerCase);
 console.log(upperCase);
-console.log(numericalValues);
+console.log(numericValue);
 console.log(specialCharacters);
+console.log(passwordCharacterArray);
