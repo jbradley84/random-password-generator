8c26fac559219baca4ece34224c0ee8ce7222f86
jbradley84 <utbradley84@yahoo.com>
2022-05-19 18:56:33 -0500

    testing fisher-yates shuffle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 // Assignment Code
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 
-// Establish possible character arrays
-// var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var numericValueArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// var specialCharactersArray = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+// Create empty password character array
 var passwordCharacterArray = [];
 
 
@@ -18,62 +14,72 @@ var passwordLength = parseInt(window.prompt("How long do you want your password 
 };*/
 
 
-// Establish password makeup
+// establish password makeup
+// confirm Y/N lowercase letters
 var lowerCase = window.confirm("Do you want your password to contain lowercase letters?"); {
+  // if TRUE, push lowercase letters into passwordCharacterArray
   if (lowerCase) {
     passwordCharacterArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
   }
 }
-// If TRUE, concatenate lowerCaseArray with passwordCharacterArray
 
 
-// Establish if uppercase letters are wanted
+// confirm Y/N uppercase letters
 var upperCase = window.confirm("Do you want your password to include uppercase letters?"); {
+  // if TRUE, push uppercase letters into passwordCharacterArray
   if (upperCase) {
     passwordCharacterArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
   }
 }
-// If TRUE, concatenate upperCaseArray with passwordCharacterArray
 
 
-// Establish if numbers are wanted
+// confirm Y/N numbers
 var numericValue = window.confirm("Do you want your password to contain numbers?"); {
+  // if TRUE, push numbers into passwordCharacterArray
   if (numericValue) {
     passwordCharacterArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
   }
 }
-// If TRUE, concatenate numericValueArray with passwordCharacterArray
 
 
-// Establish if special characters are wanted
+// confirm Y/N special characters
 var specialCharacters = window.confirm("Do you want your password to contain special characters?"); {
+  // if true, push special characters into passwordCharacterArray
   if (specialCharacters) {
     passwordCharacterArray.push("!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~");
   }
 }
-// If true, concatenate specialCharactersArray with passwordCharacterArray
 
+// figure out a way to shuffle passwordCharacterArray
+function shuffle(passwordCharacterArray) {
+  var m = passwordCharacterArray.length, t, i;
 
-// Establish password character arrays
+  // while there remain elements to shuffle...
+  while (m) {
+    // pick a remaining element...
+    i = Math.floor(Math.random() * m--);
 
-// Concatenate TRUE statement character arrays
+    // and swap it with the current element
+    t = passwordCharacterArray[m];
+    passwordCharacterArray[m] = passwordCharacterArray[i];
+    passwordCharacterArray[i] = t;
+  }
 
-// Use math.random to generate password - must pull characters from each TRUE array
-
-// Write password to the #password input
-/*function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  return passwordCharacterArray;
 }
 
+// figure out a way to pull characters from shuffled passwordCharacterArray
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+//}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
+//generateBtn.addEventListener("click", writePassword);
 console.log(passwordLength);
-console.log(lowerCase);
-console.log(upperCase);
-console.log(numericValue);
-console.log(specialCharacters);
 console.log(passwordCharacterArray);
