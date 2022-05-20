4d0519471c168951e17b2896394faa15582f354f
jbradley84 <utbradley84@yahoo.com>
2022-05-20 18:26:08 -0500

    created function to randomly sort character array, slice array to correct password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,84 +2,93 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// Create empty password character array
-var passwordCharacterArray = [];
+// function to generate random password
+var generatePassword = function () {
 
 
-// Establish password length between 8 and 128 characters
-var passwordLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8 and 128."))/*; {
+  // Establish password length between 8 and 128 characters
+  var passwordLength = parseInt(window.prompt("How long do you want your password to be? Please enter a number between 8 and 128.")); /*{
     if (passwordLength < 8 || passwordLength > 128) {
       window.alert("You must choose a password length between 8 and 128 characters long!");
     }
-};*/
+  }*/
+
+  // Create empty password character array
+  var passwordCharacterArray = [];
 
 
-// establish password makeup
-// confirm Y/N lowercase letters
-var lowerCase = window.confirm("Do you want your password to contain lowercase letters?"); {
-  // if TRUE, push lowercase letters into passwordCharacterArray
-  if (lowerCase) {
-    passwordCharacterArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-  }
+  // establish password makeup
+  // confirm Y/N lowercase letters
+  var lowerCase = window.confirm("Do you want your password to contain lowercase letters?"); {
+    // if TRUE, push lowercase letters into passwordCharacterArray
+    if (lowerCase) {
+      passwordCharacterArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    }
+  };
+
+
+  // confirm Y/N uppercase letters
+  var upperCase = window.confirm("Do you want your password to include uppercase letters?"); {
+    // if TRUE, push uppercase letters into passwordCharacterArray
+    if (upperCase) {
+      passwordCharacterArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    }
+  };
+
+
+  // confirm Y/N numbers
+  var numericValue = window.confirm("Do you want your password to contain numbers?"); {
+    // if TRUE, push numbers into passwordCharacterArray
+    if (numericValue) {
+      passwordCharacterArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    }
+  };
+
+
+  // confirm Y/N special characters
+  var specialCharacters = window.confirm("Do you want your password to contain special characters?"); {
+    // if true, push special characters into passwordCharacterArray
+    if (specialCharacters) {
+      passwordCharacterArray.push("!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~");
+    }
+  };
+  console.log(passwordCharacterArray);
+
+
+  // function to shuffle passwordCharacterArray into random order using Fisher Yates Method
+  let arr = passwordCharacterArray;
+
+  let arrayShuffle = function (arr) {
+    let newPos, temp;
+
+    for (let i = arr.length - 1; i > 0; i--) {
+      newPos = Math.floor(Math.random() * (i + 1));
+      temp = arr[i];
+      arr[i] = arr[newPos];
+      arr[newPos] = temp;
+    }
+    return passwordCharacterArray;
+  };
+
+  // create new shuffledCharacterArray using shuffle function
+  let shuffledCharacterArray = arrayShuffle(passwordCharacterArray);
+  console.log(shuffledCharacterArray);
+
+  // create randomPassword by slicing shuffledCharacterArray at value equal to passwordLength, remove commas
+  let randomPassword = shuffledCharacterArray.slice(0, passwordLength).join("");
+  console.log(randomPassword);
+  console.log(passwordLength);
 }
-
-
-// confirm Y/N uppercase letters
-var upperCase = window.confirm("Do you want your password to include uppercase letters?"); {
-  // if TRUE, push uppercase letters into passwordCharacterArray
-  if (upperCase) {
-    passwordCharacterArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-  }
-}
-
-
-// confirm Y/N numbers
-var numericValue = window.confirm("Do you want your password to contain numbers?"); {
-  // if TRUE, push numbers into passwordCharacterArray
-  if (numericValue) {
-    passwordCharacterArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-  }
-}
-
-
-// confirm Y/N special characters
-var specialCharacters = window.confirm("Do you want your password to contain special characters?"); {
-  // if true, push special characters into passwordCharacterArray
-  if (specialCharacters) {
-    passwordCharacterArray.push("!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~");
-  }
-}
-
-// figure out a way to shuffle passwordCharacterArray
-function shuffle(passwordCharacterArray) {
-  var m = passwordCharacterArray.length, t, i;
-
-  // while there remain elements to shuffle...
-  while (m) {
-    // pick a remaining element...
-    i = Math.floor(Math.random() * m--);
-
-    // and swap it with the current element
-    t = passwordCharacterArray[m];
-    passwordCharacterArray[m] = passwordCharacterArray[i];
-    passwordCharacterArray[i] = t;
-  }
-
-  return passwordCharacterArray;
-}
-
-// figure out a way to pull characters from shuffled passwordCharacterArray
 
 // Write password to the #password input
 //function writePassword() {
-  //var password = generatePassword();
- // var passwordText = document.querySelector("#password");
+//var password = generatePassword();
+// var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+//passwordText.value = password;
 
 //}
 
 // Add event listener to generate button
 //generateBtn.addEventListener("click", writePassword);
-console.log(passwordLength);
-console.log(passwordCharacterArray);
+generatePassword();
